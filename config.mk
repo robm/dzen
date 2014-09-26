e1fde848ffbee93d942d8cc0d42163464642a355
Steven Honeyman <stevenhoneyman@users.noreply.github.com>
2014-09-26 21:47:56 +0100

    config.mk improvements
    - change default to option 4 (Xft breaks some cyrillic stuff)
    - remove unnecessary explicit include of /usr/include
    - CFLAGS for all options are now additional instead of replace
    
    Signed-off-by: Steven Honeyman <stevenhoneyman@users.noreply.github.com>

--- config.mk
+++ config.mk
@@ -9,7 +9,7 @@ MANPREFIX = ${PREFIX}/share/man
 
 X11INC = /usr/X11R6/include
 X11LIB = /usr/X11R6/lib
-INCS = -I. -I/usr/include -I${X11INC}
+INCS = -I. -I${X11INC}
 
 # Configure the features you want to be supported
 # Only one of the following options has to be uncommented,
@@ -20,42 +20,42 @@ INCS = -I. -I/usr/include -I${X11INC}
 
 ## Option 1: No Xinerama no XPM no XFT
 #LIBS = -L${X11LIB} -lX11
-#CFLAGS = -Wall -Os ${INCS} -DVERSION=\"${VERSION}\"
+#CFLAGS += -Wall -Os ${INCS} -DVERSION=\"${VERSION}\"
 
 
 ## Option 2: No Xinerama with XPM
 #LIBS = -L${X11LIB} -lX11 -lXpm
-#CFLAGS = -Wall -Os ${INCS} -DVERSION=\"${VERSION}\" -DDZEN_XPM
+#CFLAGS += -Wall -Os ${INCS} -DVERSION=\"${VERSION}\" -DDZEN_XPM
 
 
 # Option 3: With Xinerama no XPM
 #LIBS = -L${X11LIB} -lX11 -lXinerama
-#CFLAGS = -Wall -Os ${INCS} -DVERSION=\"${VERSION}\" -DDZEN_XINERAMA
+#CFLAGS += -Wall -Os ${INCS} -DVERSION=\"${VERSION}\" -DDZEN_XINERAMA
 
 
 ## Option 4: With Xinerama and XPM
-#LIBS = -L${X11LIB} -lX11 -lXinerama -lXpm
-#CFLAGS = -Wall -Os ${INCS} -DVERSION=\"${VERSION}\" -DDZEN_XINERAMA -DDZEN_XPM
+LIBS = -L${X11LIB} -lX11 -lXinerama -lXpm
+CFLAGS += -Wall -Os ${INCS} -DVERSION=\"${VERSION}\" -DDZEN_XINERAMA -DDZEN_XPM
 
 
 ## Option 5: With XFT
 #LIBS = -L${X11LIB} -lX11 `pkg-config --libs xft`
-#CFLAGS = -Wall -Os ${INCS} -DVERSION=\"${VERSION}\" -DDZEN_XFT `pkg-config --cflags xft`
+#CFLAGS += -Wall -Os ${INCS} -DVERSION=\"${VERSION}\" -DDZEN_XFT `pkg-config --cflags xft`
 
 
 ## Option 6: With XPM and XFT
 #LIBS = -L${X11LIB} -lX11 -lXpm `pkg-config --libs xft`
-#CFLAGS = -Wall -Os ${INCS} -DVERSION=\"${VERSION}\" -DDZEN_XPM -DDZEN_XFT `pkg-config --cflags xft`
+#CFLAGS += -Wall -Os ${INCS} -DVERSION=\"${VERSION}\" -DDZEN_XPM -DDZEN_XFT `pkg-config --cflags xft`
 
 
 ## Option 7: With Xinerama and XFT
 #LIBS = -L${X11LIB} -lX11 -lXinerama `pkg-config --libs xft`
-#CFLAGS = -Wall -Os ${INCS} -DVERSION=\"${VERSION}\" -DDZEN_XINERAMA -DDZEN_XFT `pkg-config --cflags xft`
+#CFLAGS += -Wall -Os ${INCS} -DVERSION=\"${VERSION}\" -DDZEN_XINERAMA -DDZEN_XFT `pkg-config --cflags xft`
 
 
 ## Option 8: With Xinerama and XPM and XFT
-LIBS = -L${X11LIB} -lX11 -lXinerama -lXpm `pkg-config --libs xft`
-CFLAGS += -Wall -Os ${INCS} -DVERSION=\"${VERSION}\" -DDZEN_XINERAMA -DDZEN_XPM -DDZEN_XFT `pkg-config --cflags xft`
+#LIBS = -L${X11LIB} -lX11 -lXinerama -lXpm `pkg-config --libs xft`
+#CFLAGS += -Wall -Os ${INCS} -DVERSION=\"${VERSION}\" -DDZEN_XINERAMA -DDZEN_XPM -DDZEN_XFT `pkg-config --cflags xft`
 
 
 
